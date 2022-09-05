c7b79320b387aa4fc2d6be68fd60e19d255d6796
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-09-05 17:51:35 +0530

    Time: 55 ms (59.95%), Space: 13.9 MB (85.82%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -3,22 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        result = []
-        nums2_list = 0
-        stack = []
+        nums1[m:]=nums2[:n]
+        return nums1.sort()
+            
         
-        for i in range(m+n):
-            if i<m:
-                stack.append(nums1[i])
-                 
-            if stack and nums2_list < n :
-                if stack[0] < nums2[nums2_list]:
-                    nums1[i] = stack.pop(0)
-                else:
-                    nums1[i] = nums2[nums2_list]
-                    nums2_list += 1
-            elif stack:
-                nums1[i] = stack.pop(0)
-            else:
-                nums1[i] = nums2[nums2_list]
-                nums2_list += 1
