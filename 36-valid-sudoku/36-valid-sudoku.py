2daae139f5b1b49c836992d221d797b2c7d2aba7
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-09-08 23:52:38 +0530

    Time: 223 ms (9.09%), Space: 13.9 MB (35.22%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -1,5 +1,23 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = collections.defaultdict(set) 
+        rows = collections.defaultdict(set)
+        squares = collections.defaultdict(set) 
+        
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == '.':
+                    continue
+                if(board[i][j] in rows[i] or board[i][j] in cols[j] or board[i][j] in squares [(i//3,j//3)]): return False
+                    
+                rows[i].add(board[i][j])
+                cols[j].add(board[i][j])
+                squares[(i//3,j//3)].add(board[i][j])
+        return True
+    
+'''
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
         rows,cols,square = {},{},{}
         
         # for each rows value
@@ -29,3 +47,4 @@ class Solution:
                     else:
                         square[boxvalue] = set(board[i][j])
         return True
+'''
