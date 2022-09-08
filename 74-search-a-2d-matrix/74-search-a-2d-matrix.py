2328d14a1ccddfefa2546cbea7e4eee5268b0693
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-09-08 22:50:11 +0530

    Time: 69 ms (53.11%), Space: 14.4 MB (88.94%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,5 +1,6 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        #hashing
         '''
         d={}
         for i in range(len(matrix)):
@@ -8,6 +9,8 @@ class Solution:
         print(d)
             
         return True if target in d else False
+        '''
+        #binary search
         '''
         l = []
         for i in range(len(matrix)):
@@ -31,4 +34,44 @@ class Solution:
         result = binary_search(l, target)
         
         return True if result else False
- 
+        '''
+        # binarysearch based on only row and column index using matrix
+        # rowindex = index/n
+        #coindex = index%n
+        '''If we have a matrix = [[1,2,3],[4,5,6]], rows = 2, columns = 3
+
+Then the array will be [1,2,3,4,5,6] which means array is of length = number of rows * number of columns = 2 * 3 = 6
+
+Lets say we want element = 4 from matrix. In that case we will get it from rowIndex = 1 and columnIndex = 0 (0-based indexing)
+What about the array? We will get it from index = (rowIndex * numberOfColumns + columnIndex) => (1 * 3 + 0) => 3
+
+What about the other way around?
+
+If we get an element from index = 3 in an array, then if we convert that array to a n * m matrix, then what will be the row and column index in matrix for that same element?
+
+e.g. in array = [1,2,3,4,5,6], index = 3 gives us 4
+
+So, the same element in matrix of n * m size will be at rowIndex = (index/numberOfcolumns) = (3/3) = 1
+And the columnIndex = (index % numberOfColumns) = (3 % 3) = 0
+
+So, it means 4 will be found at matrix[1][0] which is correct for matrix = [[1,2,3],[4,5,6]]'''
+        start = 0
+        rows = len(matrix)
+        cols = len(matrix[0])
+            
+        end = (rows * cols) - 1
+        
+        while start <= end:
+            mid = (start + end) // 2
+            
+            row = int(mid / cols)
+            col = mid % cols
+
+            if(matrix[row][col] == target): return True
+            
+            if(matrix[row][col] < target):
+                start = mid + 1
+            else:
+                end = mid - 1
+                
+        return False;
