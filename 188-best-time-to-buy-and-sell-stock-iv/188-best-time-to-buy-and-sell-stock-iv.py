1a0fb1849ef1c5d06a7644fa0753b6bfa7fd2f63
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-09-10 23:42:02 +0530

    Time: 460 ms (10.53%), Space: 52 MB (8.59%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,5 +1,6 @@
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
+        '''
         if k==0: return 0
         dp = [[1000, 0] for _ in range(101)]
         for price in prices:
@@ -7,3 +8,19 @@ class Solution:
                 dp[i][0] = min(dp[i][0], price - dp[i - 1][1])
                 dp[i][1] = max(dp[i][1], price - dp[i][0])
         return dp[k][1]
+        '''
+        #Neetcode
+        @cache
+        def dfs(i, buying, leftitem):
+            if not leftitem or i >= len(prices): return 0
+            
+            m = dfs(i + 1, buying, leftitem)
+            if buying:
+                m = max(m, dfs(i + 1, not buying, leftitem) - prices[i])
+            else: 
+                m = max(m, dfs(i + 1, not buying, leftitem - 1) + prices[i])
+            return m
+        
+        ans = dfs(0, True, k)
+        
+        return ans
