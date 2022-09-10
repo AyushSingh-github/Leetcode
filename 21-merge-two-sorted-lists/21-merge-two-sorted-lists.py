4f224406f01f859e906c01f3320f544851af97fc
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-09-11 00:10:34 +0530

    Time: 34 ms (97.96%), Space: 13.9 MB (79.46%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -3,10 +3,10 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+'''
 class Solution:
-    '''
     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        
+
         head = prev = ListNode()
         get = lambda x,y: x if x.val < y.val else y
         while l1 and l2:
@@ -15,7 +15,9 @@ class Solution:
             else: l2 = l2.next
         prev.next = l1 or l2
         return head.next
-        '''    
+'''    
+'''
+    # SC=O(1)
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode])-> Optional[ListNode]:
         if list1 is None and list2 is None:
             return None
@@ -42,3 +44,22 @@ class Solution:
                 nodeB.next = temp
 
         return list1
+'''    
+class Solution:
+# SC = O(m*n) using dummy node For simplicity, we create a dummy node to which we attach nodes from lists. We iterate over lists using two-pointers and build up a resulting list so that values are monotonically increased.
+
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+#It creates a single ListNode() object and its reference is saved in both variables (cur and dummy). Dummy is used to keep the head of the new list and cur is used for saving the current last element when creating the list during the while statement
+        cur = dummy = ListNode()
+        while list1 and list2:               
+            if list1.val < list2.val:
+                cur.next = list1
+                list1, cur = list1.next, list1
+            else:
+                cur.next = list2
+                list2, cur = list2.next, list2
+                
+        if list1 or list2:
+            cur.next = list1 if list1 else list2
+            
+        return dummy.next
