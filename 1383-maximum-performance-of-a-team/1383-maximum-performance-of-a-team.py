12301c3b3a271c5ddf4b117d983f7fbb7ecb1ca6
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-09-11 17:55:58 +0530

    Time: 939 ms (9.29%), Space: 31.3 MB (35.02%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.py
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.py
@@ -3,20 +3,24 @@ class Solution:
         eng = []
         for eff,spd in zip(efficiency,speed):
             eng.append([eff,spd])
-            
+        
+        #soring based on eff to get max to min eff
         eng.sort(reverse=True)
-        print(eng)
+        #print(eng)
         
         maxPerformance,currspeed = 0,0
         minHeap = []
         
         for eff,spd in eng:
+            #if total spd of eng is more than K required then pop from heap and then reduce the total currspeed  till the K eng spd
             if len(minHeap)==k:
                 currspeed -= heapq.heappop(minHeap)
-                print(currspeed)
-                
+                #print(currspeed)
+            
+            # else keep incr the total speed of the eng and then push in the heap to get the mini eff amoang the eng but getting max Performance by max total eng speed     
             currspeed += spd
             heapq.heappush(minHeap,spd)
+            #maxi of prev Performance and the curr performance by the spd of top pop enfg speed 
             maxPerformance = max(maxPerformance, eff*currspeed)
             #print(eff*currspeed)
             
