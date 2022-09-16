7a1056372e228b16e2866d49baa3c45573c82b8c
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-09-16 23:12:33 +0530

    Time: 9809 ms (5.03%), Space: 41.2 MB (47.52%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.py
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.py
@@ -1,12 +1,32 @@
 class Solution:
-	def maximumScore(self, nums: List[int], multipliers: List[int]) -> int:
+    def maximumScore(self, nums: List[int], multipliers: List[int]) -> int:
+        n=len(nums)
+        m=len(multipliers)
+        
+        #Recursion + Memoization 
+        '''
+        dp=[[-1]*m for i in range(n)]
+        def helper(low, high, j):
+            if j==m:
+                return 0
 
-		score = [0]
-		mn = -1000*1000*1000
-		for rd, multiplier in enumerate(multipliers,1):
+            if dp[low][j]!=None:
+                return dp[low][j]
 
-			l1 =  [score[i]+num*multiplier for i,num in enumerate(nums[rd-1::-1])] +[mn]
-			l2 =  [mn]+[score[i] + num * multiplier for i, num in enumerate(nums[:-(rd+1):-1])]
-			score = [max(l1[i],l2[i])for i in range(rd+1)]
+            first=nums[low]*multipliers[j]+helper(low+1, high, j+1)
+            last=nums[high]*multipliers[j]+helper(low, high-1, j+1)
+            dp[low][j]=max(first, last)
+            return dp[low][j]
+        
+        return helper(0, n-1, 0)
 
-		return max(score)
+        # Tablulation
+        '''
+        dp=[[0]*(m+1) for i in range(m+1)]
+        for j in range(m-1, -1, -1):
+            for low in range(j, -1, -1):
+                first=nums[low]*multipliers[j]+dp[j+1][low+1]
+                last=nums[n-1-(j-low)]*multipliers[j]+dp[j+1][low]
+                dp[j][low]=max(first, last)
+        return dp[0][0]
+        
