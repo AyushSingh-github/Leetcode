7b88e69c2f16ecb4d377e583f7cabc540a7f6056
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-09-18 01:33:55 +0530

    Time: 177 ms (25.63%), Space: 16.3 MB (95.77%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.py
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.py
@@ -4,6 +4,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+'''
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
         nodes = set()
@@ -15,3 +16,20 @@ class Solution:
             nodes.add(root.val)
             return solve(root.left) or solve(root.right)
         return solve(root)
+'''
+    
+class Solution(object):
+	def findTarget(self, root, k):
+
+		visited = set()
+		stack = [root]
+
+		while stack:
+			node = stack.pop()
+
+			if node:
+				if node.val in visited: return True 
+				stack.append(node.right)
+				stack.append(node.left)
+
+				visited.add(k - node.val)
