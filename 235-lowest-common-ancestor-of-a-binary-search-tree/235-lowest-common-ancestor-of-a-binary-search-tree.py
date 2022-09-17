cc135462206eb1a0c1f4db098f454b94798ff0d6
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-09-18 02:18:38 +0530

    Time: 234 ms (5.13%), Space: 18.8 MB (23.09%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -4,7 +4,7 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
+'''
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         def traverse(root):
@@ -18,3 +18,16 @@ class Solution:
                 else:
                     return root
         return traverse(root)
+'''
+class Solution:
+	def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+		if not root or root == p or root == q:
+			return root
+
+		left = self.lowestCommonAncestor(root.left, p, q)
+		right = self.lowestCommonAncestor(root.right, p, q)
+
+		if left and right:
+			return root
+
+		return left or right 
