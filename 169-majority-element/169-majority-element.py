8f6ed147fe9eb6062020c8264645bcadf743e087
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-09-19 00:37:46 +0530

    Time: 456 ms (5.09%), Space: 15.6 MB (34.35%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,3 +1,4 @@
+'''
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         d = {}
@@ -6,4 +7,30 @@ class Solution:
         
         d = (sorted(d.items() , key = lambda x: x[1] , reverse = True))
         #print(dict(d))
-        return d[0][0]
+        return d[0][0] 
+'''
+
+class Solution:
+    def majorityElement(self, A: List[int]) -> int:
+        N = len(A)
+        count = 0
+        element = 0
+        
+        #  identifing the majority element
+        for i in A:
+            if count == 0:
+                element = i
+                
+            if i == element:
+                count += 1
+            else:
+                count -= 1
+        #  confirming that the element is majority or not      
+        count = 0
+        for i in A:
+            if element == i:
+                count += 1
+            if count > N // 2:
+                return element
+        
+        return -1
