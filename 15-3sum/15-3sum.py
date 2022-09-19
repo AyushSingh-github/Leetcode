3c66c45e3616f7101536f3fabe74099fd6f434e5
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-09-19 23:33:17 +0530

    Time: 6498 ms (10.03%), Space: 174.9 MB (5.95%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,7 +1,8 @@
+'''
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         # HashMap solution. TC = O(N^2.log(n))    SC = [O(N) + O(M)] -> hash + set space
-        '''
+        
         d={}
         s = set()
         res = []
@@ -34,38 +35,67 @@ class Solution:
         for i in s:
             res.append(list(i))
         return res
-        '''
+        
         
         #two pointer (D&C) changing the question as two sum using 
         # TC = O(N^2)  SC = O(M) auxilary space so equals O(1)
+        #Striver Concept
         
         nums.sort()
         res = []
-        for i in range(len(nums)-2):
+        
+        #example list = [-2,-2,-1,-1,-1,0,0,0,2,2,2]
+        
+        # 'i' is 'a' here and we take 'a' as constant and move only to distinct nums from [-2 -> -1 -> 0 -> 2] as the summ remain in (b+c = -a) eqn
+        
+        for i in range(len(nums)-2):  # only till last 3rd num as 'a' must have 2 nums lo and hi which make the triplet 
+            
+            #i or 'a' is the remain in eqn that we check when its in start or further away and dont repeat the same value as previous 'a'
+            
             if i==0 or (i>0 and nums[i]!=nums[i-1]):
-                lo = i+1
-                hi = len(nums)-1
-                a = -(nums[i])
+                lo = i+1            #lo is 'b'
+                hi = len(nums)-1    #hi is 'c'  in eqn  b+c = -a
+                a = -(nums[i])      #neg of a to simplify
                 
                 while lo<hi:
                     if nums[lo] + nums[hi] == a:
+                        #triplet found
                         res.append([nums[i],nums[lo],nums[hi]])
                         
+                        #incr lo and hi to its new pos and should not be same as before lo and hi value
                         while lo<hi and nums[lo]==nums[lo+1]:
                             lo+=1
                         while lo<hi and nums[hi]==nums[hi-1]:
                             hi-=1
+                        #final and diff pos from the last values 
                         lo+=1
                         hi-=1
                         
+                    # if sum of b+c < a,  that mean we need to incr the sum towards the 'a' by incr lo value towards the larger side else decr the curr sum to get value closer to 'a' by incr hi
                     elif (nums[lo]+ nums[hi]<a):
                         lo+=1
                     else:
                         hi-=1
         return res
                 
+'''            
             
             
-            
-            
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        arr=[]
+        nums.sort()
+        for i in range(len(nums)):
+            l=i+1
+            r=len(nums)-1
+            while l<r:
+                if nums[i]+nums[l]+nums[r]==0:
+                    arr.append((nums[i],nums[l],nums[r]))
+                    l+=1
+                    r-=1
+                elif nums[i]+nums[l]+nums[r]>0:
+                    r-=1
+                else:
+                    l+=1
+        return set(arr)               
             
