8f9cc589969bc58b2dc89dfb4fcfa2ac2c50bd72
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-09-20 01:10:02 +0530

    Time: 73 ms (11.66%), Space: 13.9 MB (64.52%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -3,4 +3,44 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
+        count0,count1,count2 =0,0,0
+        for i in nums:
+            if i==0:
+                count0+=1
+            elif i==1:
+                count1+=1
+            else:
+                count2+=1
+                
+        for i in range(count0):
+            nums[i]=0
+        for i in range(count0,+count0+count1):
+            nums[i]=1
+        for i in range(count0+count1, count0+count1+count2):
+            nums[i]=2
+        return nums
+'''
+#Most optimized solution using Dutch National Flag Algorithm
+
+    def sortColors(self, nums: List[int]) -> None:
+        
+        mid = low = 0
+        high = len(nums)-1
+        
+        def swap(a,b):
+            temp = nums[a]
+            nums[a]= nums[b]
+            nums[b] = temp
+        
+        while mid <= high:
+            
+            if nums[mid] == 0:
+                swap(low,mid)
+                low +=1
+            elif nums[mid] == 2:
+                swap(mid,high)
+                high -=1
+                mid-=1
+            
+            mid+=1
+'''
