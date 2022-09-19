67210ceadff3d2e19768a9d759952d437a84a430
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-09-20 00:33:38 +0530

    Time: 174 ms (36.61%), Space: 23.5 MB (87.46%) - LeetHub

--- 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.py
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.py
@@ -3,14 +3,26 @@ class Solution:
         d = {}
         for i in paths:
             files = i.split()
+            #print(len(files))
+        
             path = files[0]
+            #print(path)    #for root and dir
+            #print(files)
+            
             for index in range(1,len(files)):
+                #print(files[index].split("("))
+                
                 name, content = files[index].split("(")
+                #print(name," ",content)
+                
                 content = content[:-1]
+                #print(content)
+                
                 if content in d:
                     d[content].append(path+"/"+name)
                 else:
                     d[content] = [path+"/"+name]
+                #print(d)
         ans = []
         for i in d:
             if len(d[i])>1:
