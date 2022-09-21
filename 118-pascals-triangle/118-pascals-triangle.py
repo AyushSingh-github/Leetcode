cbcfe628601a9cf83ef9685cb12810d017da3028
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-09-21 20:37:47 +0530

    Time: 70 ms (5.65%), Space: 14 MB (18.11%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -1,17 +1,35 @@
-class Solution(object):
-    def generate(self, numRows):
-        result = []
-        for i in range(numRows):
-            if(i == 0):
-                prev = [1]
-                result.append(prev)
-            else:
-                curr = [1]
-                j = 1
-                while(j < i):
-                    curr.append(prev[j-1] + prev[j])
-                    j+=1
-                curr.append(1)
-                result.append(curr)
-                prev = curr
-        return result
+        #TO CALC ROW AND COL NUM value directly, we use (r-1)C(c-1) 
+        #TC= O(N2), SC= O(N2), to print full triangle
+        
+        #TC= O(N), SC= O(1), to get just value on row and column
+        
+        #TC= O(N), Sc= O(N), to print all row, get previous ans like 4th row
+        # 4C0 = 1, 4C1 = 4, 4C2 = 4*3/1*2. 4C3 = (4*3)*2/(1*2)*3  == 4C2 * (2/col num), ie, each rCc has equal numerator/demoninator
+        
+        #ex = 4C3 = (4*3*2)/(1*2*3), r=4, c=3,   for i = 0 to 3:  product_num *= r-i, product_deno *= i+1 = num/deno
+        
+#python brute force solution
+class Solution:
+    def generate(self, n: int) -> List[List[int]]:
+        ans = [[1]]
+        for i in range(1,n):
+            s=0
+            b=[]
+            b.append(1)
+            for j in range(i-1):
+                s = ans[i-1][j] + ans[i-1][j+1]
+                b.append(s)
+            b.append(1)
+            ans.append(b)
+        return ans
+    
+#optimized solution
+'''
+class Solution:
+    def generate(self, n: int) -> List[List[int]]:
+        ans=[[1]*(i+1) for i in range(n)]
+        for i in range(n):
+            for j in range(1,i):
+                ans[i][j]=ans[i-1][j-1]+ans[i-1][j]
+        return ans
+'''        
