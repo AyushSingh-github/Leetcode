4cfc4b245b8633389b89f512d99e41d299202ed2
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-09-21 21:06:30 +0530

    Time: 75 ms (5.18%), Space: 13.8 MB (63.84%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.py
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.py
@@ -1,8 +1,22 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
+        # TC =O(N2)
+        '''
         res = [1]
         for i in range(rowIndex):
             res = [0] + res
             for j in range(len(res) - 1):
                 res[j] = res[j] + res[j+1]
         return res
+        '''
+
+		# Nth Row of Pascal Triangle - nC0 , nC1, nC2 .......   nCn
+        #Striver Concept
+        #TC = O(N)
+        li=[1]
+        res = 1
+        for i in range(rowIndex):
+            res *= (rowIndex - i)
+            res /= (i+1)
+            li.append(int(res))
+        return li
