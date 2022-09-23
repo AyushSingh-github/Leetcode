c8ee2884e03eb53bb1593e266e14af5da98b8714
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-09-23 19:18:40 +0530

    Time: 3862 ms (19.69%), Space: 13.9 MB (62.20%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
@@ -10,9 +10,31 @@ class Solution:
         return int(bstr,2)%mod
         '''
         #bit manipulation
-        
+        '''
         mod  = 1000000007
         ans = 1
         for i in range(2, n + 1):
             ans = (ans << i.bit_length()) % mod + i     
         return ans % mod
+        '''
+        constant = 10**9 + 7
+        summation = 0
+        bit_width = 0
+        
+        # iterate from 1 to n
+        for i in range(1, n+1):
+            
+            # update binary bit width when we meet power of 2
+            if i & i-1 == 0:
+                bit_width += 1
+            
+            # use binary left rotation to implement concatenation
+            summation = summation << bit_width
+            
+            # add with current number
+            summation = summation | i
+            
+            # mod with constant defined by description
+            summation %= constant
+        
+        return summation
