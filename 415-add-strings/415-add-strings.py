db692a2a7bb7d946a3b67ccdfde14335d310e572
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-09-23 19:32:50 +0530

    Time: 112 ms (9.27%), Space: 13.9 MB (88.88%) - LeetHub

--- 415-add-strings/415-add-strings.py
+++ 415-add-strings/415-add-strings.py
@@ -1,4 +1,15 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
+        '''
         n = int(num1) + int(num2)
         return str(n)
+        '''
+        
+        integer_1 = 0
+        integer_2 = 0
+
+        for i in num1:
+            integer_1 = integer_1 * 10 + (ord(i)-48)            
+        for j in num2:
+            integer_2 = integer_2 * 10 + (ord(j) -48)
+        return str(integer_1 + integer_2)
