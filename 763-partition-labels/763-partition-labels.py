856c1e30d74c3d76d8994aa1ee27f946eed47eda
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-09-26 00:29:46 +0530

    Time: 67 ms (53.44%), Space: 13.7 MB (97.69%) - LeetHub

--- 763-partition-labels/763-partition-labels.py
+++ 763-partition-labels/763-partition-labels.py
@@ -3,17 +3,18 @@ class Solution:
         d = {}
         for i in range(len(s)):
             d[s[i]] = i
-        print(d)
+        #print(d)
         ar = []
         maxx = -1
         for i in range(len(s)):
             if maxx < d[s[i]]:
                 maxx = d[s[i]]
-                print(maxx,i)
+                #print(maxx,i)
             if i == maxx:
                 ar.append(i+1)
-        print("array till new string is sliced",ar)
+        #print("array till new string is sliced",ar)
         for i in range(len(ar)-1,0,-1):
             ar[i]=ar[i]-ar[i-1]
-        print("after deletion",ar)
+        #print("after deletion",ar)
         return ar
+    
