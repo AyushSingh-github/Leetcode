a8ee71446c214a6fab119cba9b42cca896fbacc7
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-09-25 23:28:28 +0530

    Time: 77 ms (86.34%), Space: 14.6 MB (21.40%) - LeetHub

--- 622-design-circular-queue/622-design-circular-queue.py
+++ 622-design-circular-queue/622-design-circular-queue.py
@@ -1,9 +1,10 @@
+#linked list
+'''
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val  = val
         self.next = next
         
-#linked list
 class MyCircularQueue:
 
     def __init__(self, k: int):
@@ -62,9 +63,48 @@ class MyCircularQueue:
 
     def isFull(self) -> bool:
         return self.counter==self.k
+'''
+
+#Arrays / Lists
+class MyCircularQueue:
+    
+    def __init__(self, k: int):
+        self.queue = []
+        self.size = k
         
+    def enQueue(self, value: int) -> bool:  
+        if self.isFull(): 
+            return False
+        self.queue.append(value)
+        return True
+        
+    def deQueue(self) -> bool:
+        if self.isEmpty():
+            return False
+        self.queue.pop(0)
+        return True
+        
+    def Front(self) -> int:
+        if len(self.queue) == 0:
+            return -1
+        return self.queue[0]
+        
+    def Rear(self) -> int:
+        if len(self.queue) == 0:
+            return -1
+        return self.queue[-1]
+        
+    def isEmpty(self) -> bool:
+        if len(self.queue) == 0:
+            return True
+        return False
+        
+    def isFull(self) -> bool:
+        if len(self.queue) == self.size:
+            return True
+        return False
 
-
+    
 # Your MyCircularQueue object will be instantiated and called as such:
 # obj = MyCircularQueue(k)
 # param_1 = obj.enQueue(value)
