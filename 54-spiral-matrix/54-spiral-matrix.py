84b674880757d7a9f7264a576d72451c807f2978
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-09-27 10:33:38 +0530

    Time: 60 ms (25.06%), Space: 13.9 MB (34.06%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -5,21 +5,29 @@ class Solution:
         res = []
         
         while left < right and top < bottom:
+            #from top row = left top row -> right top row
             for i in range(left,right):
                 res.append(matrix[top][i])
             top+=1
             
+            #from right column = top right column -> bottom right column
             for i in range(top,bottom):
                 res.append(matrix[i][right-1])
             right-=1
             
+            #if only one row or column it will [1 - 2- 3]  or [1
+            #                                                  2 
+            #                                                  3]
+            
             if not (left<right and top<bottom):
                 break
-                
+            
+            #from  bottom row = bottom right row -> bottom left row 
             for i in range(right-1,left-1,-1):
                 res.append(matrix[bottom-1][i])
             bottom-=1
             
+            #from left column = bottom left column -> top left column
             for i in range(bottom-1,top-1,-1):
                 res.append(matrix[i][left])
             left+=1
