5ccc63c41ebcafd1c5325544f56b2b56c4bbddba
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-09-28 10:27:56 +0530

    Time: 896 ms (27.43%), Space: 22.3 MB (25.81%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.py
+++ 838-push-dominoes/838-push-dominoes.py
@@ -1,11 +1,38 @@
 class Solution:
     def pushDominoes(self, dominoes: str) -> str:
+        #O(N2)
+        '''
         temp = str()
         
         while dominoes != temp:
             temp = dominoes
-            dominoes = dominoes.replace('R.L', 'xxx')       # <-- 1)
-            dominoes = dominoes.replace('R.', 'RR')         # <-- 2)
-            dominoes = dominoes.replace('.L', 'LL')         # <-- 2)
+            dominoes = dominoes.replace('R.L', 'xxx')
+            dominoes = dominoes.replace('R.', 'RR')
+            dominoes = dominoes.replace('.L', 'LL')
 
-        return  dominoes.replace('xxx', 'R.L')              # <-- 3)        
+        return  dominoes.replace('xxx', 'R.L')
+        '''
+        #O(N)   using Queue
+        
+        dom = list(dominoes)
+        q = deque()
+        
+        for i,d in enumerate(dom):
+            if d!='.':
+                q.append((i,d))
+                
+        while q:
+            i,d = q.popleft()
+            
+            if d == "L" and i>0 and dom[i-1] == ".":
+                q.append((i-1,"L"))
+                dom[i-1] = "L"
+            elif d == "R":
+                if i+1 < len(dom) and dom[i+1] == ".":
+                    if i+2 < len(dom) and dom[i+2] == "L":
+                        q.popleft()
+                    else:
+                        q.append((i+1,"R"))
+                        dom[i+1] = "R"
+                        
+        return str("".join(dom))
