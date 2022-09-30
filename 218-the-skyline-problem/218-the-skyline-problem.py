a6c852ee55d5c25816325a642092ea2614f4057c
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-10-01 00:46:30 +0530

    Time: 326 ms (29.64%), Space: 19.9 MB (48.18%) - LeetHub

--- 218-the-skyline-problem/218-the-skyline-problem.py
+++ 218-the-skyline-problem/218-the-skyline-problem.py
@@ -1,36 +1,22 @@
 class Solution:
-    def minMeetingRooms(self, intervals):
-        intervals.sort(key = lambda x: x[0])
-        res = 0
-        heap, l_heap = [], 0
-        for interval in intervals:
-            while heap and heap[0] <= interval[0]:
-                heapq.heappop(heap)
-                l_heap -= 1
-            heapq.heappush(heap, interval[1])
-            l_heap += 1
-            res = max(res, l_heap)
-        return res
-	
-    inf = 999999
     def getSkyline(self, buildings):
-        buildings.append([float('inf'), float('inf'), 0])
-        heap = []
-        key_points = []
-        curr_max = 0
+        points = []
         for Li, Ri, Hi in buildings:
-            while heap and heap[0][0] < Li:
-                ri, hi = heapq.heappop(heap)
-                heap_max = 0
-                for j in range(len(heap)):
-                    heap_max = max(heap_max, heap[j][1])
-                if heap_max < curr_max:
-                    curr_max = heap_max
-                    key_points.append([ri, curr_max])
-            if Hi > curr_max:
-                if key_points and Li == key_points[-1][0]:
-                    key_points.pop()
-                key_points.append([Li, Hi])
-                curr_max = Hi
-            heapq.heappush(heap, (Ri, Hi))
+            points.append((Li, -Hi, 1))
+            points.append((Ri, Hi, -1))
+        points.sort()
+        pq, max_height = [0], 0
+        key_points = []
+        for x, h, s in points:
+            if s == 1: # start point
+                if -h > max_height:
+                    max_height = -h
+                    key_points.append([x, -h])
+                bisect.insort_right(pq, -h)
+            else: # end point
+                pq.pop(bisect.bisect_left(pq, h))
+                pq_max = pq[-1]
+                if pq_max < max_height:
+                    max_height = pq_max
+                    key_points.append([x, max_height])
         return key_points
