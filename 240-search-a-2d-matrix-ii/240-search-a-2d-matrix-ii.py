ce8b87817908add9605b9fe9d51977b416cb3b58
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-10-02 00:48:24 +0530

    Time: 436 ms (12.38%), Space: 20.6 MB (12.62%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
@@ -24,25 +24,18 @@ class Solution:
         
         n = len(matrix)
         
-        #We start from the top right corner of the matrix.
+        #start from the top right [row][col] point of the matrix.
         row = 0
         col = len(matrix[0])-1
-        
-        # In this way, on the bottom side, all elements are bigger than current element
-        # At left side, all elements are smaller than the current element
         
         while(row < n and col >= 0):
             mid = matrix[row][col]
             
-            #If the element at this position is the target element we return True
             if(mid == target): 
                 return True
             
-            # If the element is smaller than this position that means, we need to go to left side of current element
             if(mid > target): 
                 col -= 1
-                
-            #Otherwise we need to move to bottom side
             else: 
                 row += 1
         
