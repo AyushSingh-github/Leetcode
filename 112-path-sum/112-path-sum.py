6b713a6e279420dd607748a45b6938d2dfeb6e35
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-10-04 22:38:39 +0530

    Time: 103 ms (6.36%), Space: 15.1 MB (56.49%) - LeetHub

--- 112-path-sum/112-path-sum.py
+++ 112-path-sum/112-path-sum.py
@@ -6,30 +6,8 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        #dfs recursion
-        '''
         if not root: return
         if root.val == targetSum and root.left is None and root.right is None:
             return True
         
         return self.hasPathSum(root.left, targetSum - root.val) or self.hasPathSum(root.right, targetSum-root.val)
-        '''
-        
-        #iteration
-        if root:
-            stack = [[root,targetSum - root.val]]
-        else:
-            return []
-        
-        while stack:
-            currnode, currsum = stack.pop() 
-            if (not currnode.left and not currnode.right and currsum == 0):
-                return 1
-            
-            if currnode.left:
-                stack.append([currnode.left , currsum - currnode.left.val])
-                
-            if currnode.right:
-                stack.append([currnode.right, currsum - currnode.right.val])
-                
-        return 0
