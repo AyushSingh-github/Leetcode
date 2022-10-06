b19b896ed4c4d5a89ba14281475e838b53b34bb3
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-10-07 00:18:37 +0530

    Time: 1483 ms (41.89%), Space: 70.3 MB (90.00%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.py
+++ 981-time-based-key-value-store/981-time-based-key-value-store.py
@@ -1,3 +1,4 @@
+'''
 class TimeMap:
 
     def __init__(self):
@@ -26,7 +27,33 @@ class TimeMap:
                 r = m - 1
 
         return ans
+'''
+class TimeMap:
 
+    def __init__(self):
+		# Creating a default dictionary using collections package (build-in package)
+        self.dictionary = collections.defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+		# appending (value, timestamp) tuple to the list associated to the key
+		# Remember this is a defaultdict with default value as list. So each key has an empty list which does not exist in the hash table
+		# Therefore, we directly appended the tuple without checking whether the key exist or not.
+        self.dictionary[key].append((value, timestamp))
+
+    def get(self, key: str, timestamp: int) -> str:
+		# Accessing the list associated with the key.
+        data = self.dictionary[key]
+		# If the key does not exist in our hash table, we will return ""
+        if not data: return ""
+		# Going through each timestamp in reverse order and check the below condition
+		# timestamp_prev <= timestamp
+		# if this condition is satisfied, we simply return the value associated with that perticular tuple
+        for val,time in reversed(data):
+            if time <= timestamp:
+                return val
+		# else at the end no timestamp exist in the list which is less than the given timestamp, so we return
+        return ""
+    
 
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
