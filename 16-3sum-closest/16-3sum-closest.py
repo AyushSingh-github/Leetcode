e8d00ae7f21a41ff23070390d5df6d9313b63c83
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-10-08 21:32:24 +0530

    Time: 5063 ms (78.79%), Space: 14.3 MB (12.36%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.py
+++ 16-3sum-closest/16-3sum-closest.py
@@ -28,6 +28,8 @@ class Solution:
             find(i,j,k)
         return res[0]
 '''
+#O(N2) =  two pointer + sorting 
+'''
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         
@@ -51,3 +53,30 @@ class Solution:
                     else:
                         high -= 1
         return ans
+'''
+import sys
+class Solution:
+	def threeSumClosest(self, nums: List[int], target: int) -> int:
+		nums.sort()
+		n = len(nums)
+		ans = 0
+		diff = sys.maxsize
+
+		for i in range(n):
+			l , r = i + 1, n - 1
+
+			while l < r:
+				currSum = nums[i] + nums[l] + nums[r]
+
+				if currSum == target:
+					return currSum
+
+				if currSum > target:
+					r -= 1
+				else:
+					l += 1
+
+				if abs(target - currSum) < diff:
+					ans = currSum
+					diff = abs(target - currSum)
+		return ans
