9327226ded228f205b8be78db93c57ef3e7ca58d
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-10-12 00:36:36 +0530

    Time: 192 ms (76.80%), Space: 19.1 MB (10.07%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,19 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key = lambda x:(x[0],x[1]))
+        ans= []
+        
+        #previous interval 1st and 2nd value and compare with next interval 1st value and then 2nd value
+        low = intervals[0][0]
+        high =  intervals[0][1]
+        for i in range(1,len(intervals)):
+            if low <= intervals[i][0] <= high and intervals[i][1] >= high:
+                high = intervals[i][1]
+            elif low <= intervals[i][0] <= high and intervals[i][1] < high:
+                continue
+            else:
+                ans.append([low,high])
+                low = intervals[i][0]
+                high = intervals[i][1]
+        ans.append([low,high])
+        return ans
