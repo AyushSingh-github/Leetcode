b92a0c0764185075a81f800a458213e30a3b739e
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-10-13 00:23:58 +0530

    Time: 91 ms (58.47%), Space: 14 MB (21.21%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
@@ -1,7 +1,8 @@
 class Solution:
+    #O(N2)
+    '''
     def finalPrices(self, prices: List[int]) -> List[int]:
         n=len(prices)
-    
         for i in range(n-1):
             for j in range(i+1,n):
                 if prices[i]>=prices[j]:
@@ -9,3 +10,20 @@ class Solution:
                     break
     
         return prices
+        '''
+        
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        stack = []
+        for i in range(len(prices)):
+            
+            # comparing the previous element with current element as descibed in the question to calculate the discount.
+            while stack and (prices[stack[-1]] >= prices[i]):
+                
+                # reducing the price of the current elem from previous.
+                prices[stack.pop()] -= prices[i]
+                
+            #In stack we`ll just stores the index of the prices. Using those indexes will make changes in the prices list itself.
+            stack.append(i) 
+            
+        return prices 
