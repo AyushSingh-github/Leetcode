deb5eb414f8927e9db6fb7e1c4d432b5adfbc8d8
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-10-15 23:39:50 +0530

    Time: 5002 ms (28.22%), Space: 368.1 MB (40.49%) - LeetHub

--- 1531-string-compression-ii/1531-string-compression-ii.py
+++ 1531-string-compression-ii/1531-string-compression-ii.py
@@ -119,19 +119,22 @@ class Solution:
             
 '''
 
-'''
+#3
 class Solution:
     def getLengthOfOptimalCompression(self, s: str, k: int) -> int:
         @cache
         def dp(i, prev, prev_cnt, k):
+            
             # set it to inf as we will take the min later
             if k < 0: return inf
+            
             # we delete all characters, return 0
             if i == len(s): return 0
             # here we can have two choices, we either
             # 1. delete the current char
             # 2. keep the current char
             # we calculate both result and take the min one
+            
             delete = dp(i + 1, prev, prev_cnt, k - 1)
             if s[i] == prev:
                 # e.g. a2 -> a3
@@ -145,10 +148,12 @@ class Solution:
                 # e.g. prev_cnt = 3: a3 -> a4
                 # e.g. prev_cnt = 8: a8 -> a9
                 # alternative you can calculate `RLE(prev_cnt + 1) - RLE(cnt)`
+                
                 if prev_cnt in [1, 9, 99]:
                     keep += 1
             else:
                 # e.g. a
+                
                 keep = dp(i + 1, s[i], 1, k) + 1
             return min(delete, keep)
         
@@ -156,9 +161,10 @@ class Solution:
         # starting from index i 
         # with previous character `prev`
         # with `prev_cnt` times repeated so far
-        return dp(0, "", 0, k)
-    '''
+        return dp(0, "-1", 0, k)
+    
 #4.
+'''
 from functools import lru_cache
 from math import inf
 
@@ -194,3 +200,4 @@ class Solution:
             return min(1 + counts(k, i + 1, i, 1), counts(k - 1, i + 1, j, c))
 
         return counts(k, 0, -1, 0)
+'''
