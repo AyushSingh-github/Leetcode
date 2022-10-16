a53f73bb2e0fc8ee86c7079bb299ccad387848f4
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-10-17 00:16:31 +0530

    Time: 2096 ms (45.57%), Space: 15.5 MB (12.70%) - LeetHub

---
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
+        @lru_cache(1000)
+        def solve(i,j):
+            if j == 1 and i < len(jobDifficulty):
+                return max(jobDifficulty[i:])
+            else:
+                ans = 99999
+                maxx = 0
+                for k in range(i, len(jobDifficulty)):
+                    maxx = max(maxx, jobDifficulty[k])
+                    ans = min(ans, maxx + solve(k+1, j-1))
+                    # print(ans,i,j,maxx)
+                return ans
+            
+        ans = solve(0,d) 
+        if ans != 99999:
+            return ans
+        return -1
