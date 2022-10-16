1bfb687e95fdffea1c56716785c6364226aa1ce6
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-10-17 00:18:09 +0530

    Time: 176 ms (63.30%), Space: 17 MB (96.65%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,34 @@
+'''
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d = {}
+        for i in range(len(strs)):
+            #print(''.join(sorted(strs[i])))
+            d[i] = ''.join(sorted(strs[i]))
+        print(d)
+'''
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        idx_map = {}
+        res = []
+        sorted_strs=[]
+        
+        for i in range(len(strs)):
+            sorted_strs.append(''.join(sorted(strs[i])))
+        #print(sorted_strs)
+        
+        for index, word in enumerate(sorted_strs):
+            if word in idx_map:
+                
+			    # get the original string values as list into the hashmap of key, value pair of sorted_strs:list of strs values at that same index
+                
+                idx_map[word].append(strs[index])
+            else:
+                idx_map[word] = [strs[index]]
+        #print(idx_map)
+                
+        for entry in idx_map.values():
+            res.append(entry)
+        
+        return res
+    
