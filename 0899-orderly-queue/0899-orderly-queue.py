65ba95a714c79665bcde9acfc4fdb91a5b9266d8
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-11-06 23:55:11 +0530

    Time: 64 ms (44.05%), Space: 14 MB (39.29%) - LeetHub

---
+++ 0899-orderly-queue/0899-orderly-queue.py
@@ -0,0 +1,15 @@
+class Solution:
+    def orderlyQueue(self, s: str, k: int) -> str:
+        if k>1:
+            return ''.join(sorted(s))     # O(nlogn)
+        
+        n = len(s)
+        #if n == 1:
+        #    return s
+        m = s
+        
+        # ---O(n^2)----
+        for i in range(n):         # O(n)
+            s = s[1:] + s[0]
+            m = min(m,s)         # O(n)
+        return m
