01321b6cc9ce63947a6466baaecdea63208a0e29
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-11-07 22:36:21 +0530

    Time: 37 ms (86.05%), Space: 13.8 MB (54.69%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.py
+++ 1323-maximum-69-number/1323-maximum-69-number.py
@@ -1,7 +1,8 @@
 class Solution:
     def maximum69Number (self, num: int) -> int:
+        #using string:      TC-> O(N)
+        '''
         s = str(num)
-        
         if s[0] == '6':
             return str(9)+s[1:]
 
@@ -9,5 +10,17 @@ class Solution:
             if s[i] == '6':
                 return s[:i]+str(9)+s[i+1:]
                 break
-        
         return s
+        '''
+        
+        #using maths
+        curr_index,indexsix,copy = 0,-1,num
+        
+        while copy:
+            if copy%10 == 6:
+                indexsix = curr_index
+            
+            copy //= 10
+            curr_index += 1
+            
+        return  num if indexsix == -1 else num + 3*10**indexsix   # [9669 + 3*10*2 (300)] = 9969
