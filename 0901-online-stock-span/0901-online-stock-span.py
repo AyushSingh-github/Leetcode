bbb00995b5a1d486865e03a5fe8fa83748a6be1c
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-11-10 00:09:21 +0530

    Time: 1081 ms (29.82%), Space: 20.8 MB (5.03%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.py
+++ 0901-online-stock-span/0901-online-stock-span.py
@@ -12,7 +12,10 @@ class StockSpanner:
         self.stack.append([price, ans])
         return ans
 '''
+
+#a different trick for dealing with the corner cases: add 1 pair in the initial stack. the ans = 1 and add up in the standard solution may be a little bit hard to understand.
 class StockSpanner:
+    '''
     def __init__(self):
         self.monostack = [(1e10, -1)]
         self.index = -1
@@ -28,7 +31,33 @@ class StockSpanner:
     
         self.monostack.append([price, self.index])
         return self.index - self.monostack[-2][1]
-    
+    '''
+    def __init__(self):
+        self.ledger = [-1]
+        self.jump = dict()
+        
+    def next(self, price: int) -> int:
+        # add price to ledger and add entry for jump
+        self.ledger.append(price)
+        p_idx = len(self.ledger)-1
+        if p_idx == 1:
+            return 1 # first entry
+        
+        # check previous val
+        prev = self.ledger[p_idx-1]
+        prev_idx = p_idx-1
+        
+        if prev <= price:
+            while prev <= price and prev_idx > 0:
+                if prev_idx in self.jump:
+                    prev_idx = self.jump[prev_idx]
+                else:
+                    prev_idx -= 1
+                prev = self.ledger[prev_idx]
+            self.jump[p_idx] = prev_idx+1
+            return p_idx-prev_idx
+        else:
+            return 1
 # Your StockSpanner object will be instantiated and called as such:
 # obj = StockSpanner()
 # param_1 = obj.next(price)
