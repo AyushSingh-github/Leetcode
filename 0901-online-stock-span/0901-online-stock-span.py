be33a2d3853e92eec87cd27e702d5d0887269fb0
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-11-10 00:07:21 +0530

    Time: 935 ms (57.07%), Space: 19.2 MB (95.82%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.py
+++ 0901-online-stock-span/0901-online-stock-span.py
@@ -1,3 +1,5 @@
+#monotonic stack
+'''
 class StockSpanner:
     def __init__(self):
         self.stack = []
@@ -9,7 +11,24 @@ class StockSpanner:
         
         self.stack.append([price, ans])
         return ans
-
+'''
+class StockSpanner:
+    def __init__(self):
+        self.monostack = [(1e10, -1)]
+        self.index = -1
+    
+    def next(self, price: int) -> int:
+        self.index += 1
+        # if self.monostack[-1][0] > price:
+        #     self.monostack.append([price, self.index])
+        #     return 1
+    
+        while self.monostack[-1][0] <= price:
+            self.monostack.pop()
+    
+        self.monostack.append([price, self.index])
+        return self.index - self.monostack[-2][1]
+    
 # Your StockSpanner object will be instantiated and called as such:
 # obj = StockSpanner()
 # param_1 = obj.next(price)
