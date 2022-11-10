33ad738ec8b282fce1514f1a8c2a6d89b4266839
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-11-10 22:10:14 +0530

    Time: 66 ms (24.48%), Space: 13.8 MB (74.80%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -0,0 +1,29 @@
+#TC-> O(m+n),   SC-> O(m+n)
+
+class Solution:
+    def backspaceCompare(self, S, T):
+        def build(S):
+            ans = []
+            for c in S:
+                if c != '#':
+                    ans.append(c)
+                elif ans:
+                    ans.pop()
+            return "".join(ans)
+        return build(S) == build(T)
+
+#TC-> O(m+n),   SC-> O(1)    
+    '''
+    def backspaceCompare(self, S, T):
+        def F(S):
+            skip = 0
+            for x in reversed(S):
+                if x == '#':
+                    skip += 1
+                elif skip:
+                    skip -= 1
+                else:
+                    yield x
+
+        return all(x == y for x, y in itertools.izip_longest(F(S), F(T)))
+        '''
