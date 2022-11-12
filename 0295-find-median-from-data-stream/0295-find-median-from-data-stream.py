6629e85d3ac08b649c7ae2c7472a652ca660ba02
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-11-13 00:07:31 +0530

    Time: 2459 ms (11.26%), Space: 36 MB (57.44%) - LeetHub

---
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -0,0 +1,18 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.data = []
+
+    def addNum(self, num: int) -> None:
+        # We're maintaining a monotone increasing list by inserting element using binary search
+        bisect.insort(self.data, num)
+
+    def findMedian(self) -> float:
+        n = len(self.data)
+        return self.data[n//2] if n&1 else (self.data[n//2] + self.data[n//2-1]) / 2
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
