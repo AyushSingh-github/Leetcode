f2f8d71443f4611f3661734c686e14ff3a175efc
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-11-15 22:31:12 +0530

    Time: 214 ms (19.21%), Space: 21.5 MB (46.74%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -4,8 +4,45 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+'''
 class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
         if root:
             return self.countNodes(root.left)+ self.countNodes(root.right) + 1
         return 0
+'''
+
+#First we calculate the height of the tree by reaching the leftmost node. Then we calculate the number of node empty leaf nodes using reverse preorder traversal. Why? because we only need to find the number of empty leaves and we know the complete binary tree fills from left, so we count from right.
+
+#How does it take < O(n)? Since we only process till nodes at maxheight - 1 and we dont process the leaf nodes, we also break once we find a non-empty leaf node.
+
+
+
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        if not root: return 0
+        
+        node = root
+        height = 0
+        
+        while node: # find height
+            height += 1
+            node = node.left
+        
+        stack = [(root, 1)]
+        empties = 0
+        
+        while stack: # find empty leaf nodes
+            node, h = stack.pop()
+            
+            if h == height - 1:
+                if node.right is None: empties += 1
+                else: break
+                if node.left is None: empties += 1
+                else: break
+                continue
+            
+            if node.left: stack.append((node.left, h + 1))
+            if node.right: stack.append((node.right, h + 1))
+        
+        return 2 ** height - 1 - empties 
