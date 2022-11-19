afdbbb82579005e29883068fff8c4f3d14d466b4
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-11-19 20:45:35 +0530

    Time: 95 ms (54.30%), Space: 13.9 MB (30.67%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,37 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        romans = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        
+        n = len(s)
+        integer = 0 # running sum
+        last_value = 0 # keep track of last value
+        
+        # Iterate backwards
+        for i in range(1,n+1):
+            value = romans[s[-i]]
+            
+            # when the new value is larger or equal to previous, just add to sum
+            if value >= last_value:
+                integer += value
+            
+            # Else you must subtract the value from the sum
+            else:
+                integer -= value
+            
+            # update the last value seen
+            last_value = value
+        return integer
+    
+
+'''
+        total = 0
+        i = 0
+        while i < len(s):
+            if i+1<len(s) and romans[s[i]] < romans[s[i+1]]:
+                total+=romans[s[i+1]]-romans[s[i]]
+                i+=1
+            else:
+                total+=romans[s[i]]
+            i+=1
+        return total
+'''
