0731ced5ffc2ae512ede756d89415608460c47e8
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-11-19 20:32:21 +0530

    Time: 871 ms (22.89%), Space: 14.9 MB (26.51%) - LeetHub

--- 0587-erect-the-fence/0587-erect-the-fence.py
+++ 0587-erect-the-fence/0587-erect-the-fence.py
@@ -1,7 +1,7 @@
 class Solution:
     def outerTrees(self, trees: List[List[int]]) -> List[List[int]]:
         # Gram Scan Convex Hull
-        
+        '''
         def compare_slopes(p1, p2, p3):
             x1, y1 = p1
             x2, y2 = p2
@@ -18,4 +18,27 @@ class Solution:
             upper.append(tuple(point))  
             
         return list(set(lower + upper))
+        '''
         
+        #Monotone Chain Convex Hull
+
+        trees = sorted(trees, key = lambda e:(e[0],e[1]))
+        if len(trees) <= 1:
+            return trees
+        
+        #checking orientation of vectors oa and ob
+        def ccw(o,a,b):
+            return (a[0]-o[0])*(b[1]-o[1])-(b[0]-o[0])*(a[1]-o[1]) > 0
+        lower_chain = []
+        
+        for p in trees:
+            while len(lower_chain) > 1 and ccw(lower_chain[-1], lower_chain[-2], p):
+                lower_chain.pop()
+            lower_chain.append(p)
+        upper_chain = []
+        
+        for p in reversed(trees):
+            while len(upper_chain) > 1 and ccw(upper_chain[-1], upper_chain[-2], p):
+                upper_chain.pop()
+            upper_chain.append(p)
+        return set([(e[0],e[1]) for e in lower_chain[:-1]+upper_chain[:-1]])
