a391157b590ef5afbaf99666416c4a635944797b
Ayush Singh <70777486+AyushSingh-github@users.noreply.github.com>
2022-11-28 19:47:07 +0530

    Time: 2247 ms (82.18%), Space: 70.8 MB (5.43%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -1,5 +1,5 @@
 #Set,    TC-> O(N.logN),    SC-> O(N)
-
+'''
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
         zero_loss = set()
@@ -26,7 +26,7 @@ class Solution:
                 one_loss.add(loser)          
             
         return [sorted(list(zero_loss)), sorted(list(one_loss))]
-
+'''
 
 
 #hashMap + Set,   TC-> O(N.logN), SC-> (N)
@@ -97,3 +97,34 @@ class Solution:
                 
         return answer
 '''
+
+#You can also use max(loss.keys) instead of max(chain(*matches)) but it was much slower for me.
+
+#Algorithm:
+#Count losses in matches using a hashmap.
+
+#Iterate through all integers until largest in matches, only add integers in our hashmap and with a value less than or equal to 1 to our result output.
+
+#Return our result! Range of integers is already sorted, therefore no need for a wasteful sorted() function call.
+
+class Solution:
+    def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+        loss, res = {}, [[], []]
+        for winner, loser in matches:
+            loss[winner] = loss.get(winner, 0)
+            loss[loser] = loss.get(loser, 0) + 1
+        for player in range(max(chain(*matches)) + 1):
+            if player in loss and loss[player] <= 1:
+                res[loss[player]].append(player)
+        return res
+
+#Complexity Analysis:
+#Let n be the size of our input array matches:
+
+#Time Complexity: O(n)
+#We iterate through matches and count up losses and then iterate up to the largest integer in matches, "k". k is an integer so this simplifies to O(n).
+
+#Space Complexity: O(n)
+#loss and res each take up n space, 2n simplifies to n space
+
+#We do not need to generate k arbitrary space for players not in matches nor hardcode an arbitrary maximum set in the constraints.
